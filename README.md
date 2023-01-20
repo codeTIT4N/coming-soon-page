4710c16e4bc7589ec8e3a002bddf6a18af367274
codeTIT4N <lokeshkumar636@yahoo.com>
2023-01-21 03:33:31 +0530

    updated coming soon page according to need

--- README.md
+++ README.md
@@ -1,17 +1,6 @@
-coming-soon
-======================
-[![deploy](https://github.com/arkn98/coming-soon/workflows/deploy/badge.svg)](https://github.com/arkn98/coming-soon/actions?query=workflow%3Adeploy)
-[![license](https://img.shields.io/github/license/arkn98/coming-soon)](LICENSE)
-
 A simple coming soon page, built using [create-react-app](https://github.com/facebook/create-react-app).
 
-<p align="center">
-  <img src="./docs/scrot.png" alt="Coming soon page" width="738">
-</p>
-
-**Note:** I created this as an exercise when I was learning React. So please be mindful of many things that are considered "non-optimal".
-
-## Getting started
+## How to use
 
 ### Installation
 
@@ -29,8 +18,10 @@ A simple coming soon page, built using [create-react-app](https://github.com/fac
 1. `yarn build` to build the app
 2. Copy the contents of the `build` folder and serve it with your own server
 
-## Thanks
-
-Uses: 
- - [simple-icons](https://github.com/simple-icons/simple-icons) released under the [CC0-1.0](https://github.com/simple-icons/simple-icons/blob/develop/LICENSE.md) license.
+## Uses: 
+ - [iconmonstr](https://iconmonstr.com/)
  - [IBM Plex](https://github.com/IBM/plex) released under the [OFL-1.1](https://github.com/IBM/plex/blob/master/LICENSE.txt) license.
+
+ ## Credits
+- Original code by: https://github.com/arkn98
+- Modified by me: https://github.com/codeTIT4N
--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ReactComponent as IconFacebook } from './assets/icons/facebook.svg';
+import { ReactComponent as IconLinkedin } from './assets/icons/linkedin.svg';
 import { ReactComponent as IconTwitter } from './assets/icons/twitter.svg';
 import { ReactComponent as IconGithub } from './assets/icons/github.svg';
 import "./App.css";
@@ -10,31 +10,28 @@ class App extends React.Component {
       <div className="card">
         <div className="header">
           <div className="logo">
-            <a href=".">LOGO</a>
+            <a href=".">LokeshInTech.com</a>
           </div>
           <div className="social">
-            <a href="https://facebook.com" title="Facebook" target="_blank" rel="noopener noreferrer">
-              <IconFacebook className="icon" />
-            </a>
-            <a href="https://twitter.com" title="Twitter" target="_blank" rel="noopener noreferrer">
-              <IconTwitter className="icon" />
-            </a>
-            <a href="https://github.com/arkn98/coming-soon" title="GitHub" target="_blank" rel="noopener noreferrer">
+            <a href="https://github.com/codeTIT4N" title="GitHub" target="_blank" rel="noopener noreferrer">
               <IconGithub className="icon" />
+            </a>
+            <a href="https://www.linkedin.com/in/lokesh-kumar-nalot-0baa691b9/" title="Linkedin" target="_blank" rel="noopener noreferrer">
+              <IconLinkedin className="icon" />
+            </a>
+            <a href="https://twitter.com/lokeshtweets_" title="Twitter" target="_blank" rel="noopener noreferrer">
+              <IconTwitter className="icon" />
             </a>
           </div>
         </div>
         <div className="content">
           <div className="title-holder">
-            <h1>Get ready for the change.</h1>
-            <p>Website coming soon. Please check back to know more. Shoot us an email if you're curious.</p>
+            <h1>Coming Soon</h1>
+            <p>Website coming soon. Please check back to know more. Shoot me an email if you're curious.</p>
           </div>
-          <a href="mailto:user@example.com">
-            <div className="cta">Send us an email</div>
+          <a href="mailto:lokeshkumar636@yahoo.com">
+            <div className="cta">Send me an email</div>
           </a>
-        </div>
-        <div className="footer">
-          <span>made by <a className="underlined" href="https://github.com/arkn98" target="_blank" rel="noopener noreferrer">arkn98</a> using <a className="underlined" href="https://reactjs.org/" title="ReactJS" target="_blank" rel="noopener noreferrer">React</a> | <a className="underlined" href="https://github.com/arkn98/coming-soon" title="GitHub repo" target="_blank" rel="noopener noreferrer">GitHub</a></span>
         </div>
       </div>
     );
