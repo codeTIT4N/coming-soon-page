5c567bd1d01c40cd4bec740cd5a843bd9c1192e2
codeTIT4N <lokeshkumar636@yahoo.com>
2023-01-28 23:12:25 +0530

    added hashnode and telegram logo and some restructuring

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { ReactComponent as IconTwitter } from './assets/icons/twitter.svg';
 import { ReactComponent as IconGithub } from './assets/icons/github.svg';
 import { ReactComponent as IconHashnode } from './assets/icons/hashnode.svg';
 import { ReactComponent as IconYoutube } from './assets/icons/youtube.svg';
+import { ReactComponent as IconTelegram } from './assets/icons/telegram.svg';
 import "./App.css";
 
 class App extends React.Component {
@@ -12,7 +13,7 @@ class App extends React.Component {
       <div className="card">
         <div className="header">
           <div className="logo">
-            <a href=".">LokeshKr.com</a>
+            <a href=".">Lokesh Kumar</a>
           </div>
         </div>
         <div className="content">
@@ -32,14 +33,17 @@ class App extends React.Component {
             <a href="https://www.linkedin.com/in/lokesh-kumar-nalot-0baa691b9" title="Linkedin" target="_blank" rel="noopener noreferrer">
               <IconLinkedin className="icon" />
             </a>
+            <a href="https://blog.lokeshkr.com" title="Hashnode" target="_blank" rel="noopener noreferrer">
+              <IconHashnode className="icon" />
+            </a>
+            <a href="https://t.me/lokesshk" title="Telegram" target="_blank" rel="noopener noreferrer">
+              <IconTelegram className="icon" />
+            </a>
             <a href="https://twitter.com/lokeshtweets_" title="Twitter" target="_blank" rel="noopener noreferrer">
               <IconTwitter className="icon" />
             </a>
             <a href="https://www.youtube.com/channel/UCVWq-83WQElIoIN6NGdCXLw" title="Youtube" target="_blank" rel="noopener noreferrer">
               <IconYoutube className="icon" />
-            </a>
-            <a href="https://blog.lokeshkr.com" title="Hashnode" target="_blank" rel="noopener noreferrer">
-              <IconHashnode className="icon" />
             </a>
           </div>
         </div>
